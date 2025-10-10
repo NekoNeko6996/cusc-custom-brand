b8016a59b6236850a915b3895586dce2df4f53c6
nekoneko6996 <lqm231231@gmail.com>
2025-10-10 13:57:47 +0700

    - add export logo

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 export { primaryNav, secondaryNav } from './navigation';
 export { footerLinks } from './footers';
 // Kèm export đường dẫn SCSS để MFE có thể `import '@edx/brand/paragon/build/scss/core.scss'` như cũ
+export { default as tutorLogo } from './assets/tutor-logo.png';
+export { default as openedxLogo } from './assets/openedx-logo.png';
+export { default as ctuLogo } from './assets/ctu-logo.png';
+export { default as cuscLogo } from './assets/cusc-logo.png';
