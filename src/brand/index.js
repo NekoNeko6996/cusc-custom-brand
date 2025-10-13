74a2aee491237b93376c36c88515fa3866ff251f
nekoneko6996 <lqm231231@gmail.com>
2025-10-13 12:14:05 +0700

    brand: add default nav and logo exports

---
+++ src/brand/index.js
@@ -0,0 +1,19 @@
+import ctuLogo from '../assets/ctu-logo.png';
+
+export const primaryNav = [
+  { href: '/courses', content: 'Courses' },
+  {
+    type: 'menu',
+    content: 'More',
+    items: [
+      { href: '/about', content: 'About' },
+      { href: '/contact', content: 'Contact' },
+    ],
+  },
+];
+
+export const secondaryNav = [
+  { href: '/help', content: 'Help' },
+];
+
+export { ctuLogo };
